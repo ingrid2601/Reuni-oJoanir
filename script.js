2da33ce21cb2d146c0addc8ad7098ab47993187a
ingrid2601 <159648748+ingrid2601@users.noreply.github.com>
2025-06-25 15:05:59 -0300

    .

--- script.js
+++
@@ -1,6 +0,0 @@
-function inserirNome (){
-    let nomeUsuário = prompt ("Qual é o seu nome");
-    let elemento = document . querySelector ("#nome-usuario");
-    elemento.textContent = nomeUsuário
-}
-inserirNome();
